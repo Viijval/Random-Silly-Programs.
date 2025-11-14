8de8d0223b6e3159dadbb9e55d781e261ea854f2
Viijval <159607092+Viijval@users.noreply.github.com>
2025-11-14 23:40:22 +0530

    bread. tkinter testing

---
+++ program.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+window=tk.Tk()
+window.geometry('720x480')
+window.title('Bookie!')
+icon=tk. PhotoImage(file='C:\V\\art\\bread.jpg')
+window.iconphoto(True,icon)
+window.config(background='Blue')
+window.mainloop()
